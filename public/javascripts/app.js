8533080026134c0818f03b69e6e4c6056d7e8b59
Riza Khan <khanriza@gmail.com>
2019-09-18 16:02:10 -0400

    first commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('.hamburger')
+const nav = document.querySelector('.nav-ul')
+
+button.addEventListener('click', () => {
+
+    nav.classList.toggle('delete')
+    
+})
+
